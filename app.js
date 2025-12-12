8c27af5a324ff7592ef16f3dd140961b920263ee
arieljchase <200566592@student.georgianc.on.ca>
2025-12-11 19:59:02 -0500

    tested endpoint CRUD functionality in insomnia, can successfully get, post and delete :)

--- app.js
+++ app.js
@@ -4,36 +4,43 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
-const config = require('./config/config');   // ✅ FIXED PATH
+const config = require('./config/config'); 
 
+// Routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const restaurantRouter = require('./routes/restaurants');
 
+// Create Express app FIRST
+var app = express();
+
+// Connect to MongoDB
 mongoose.connect(config.db)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log("Database connection error: ", err));
 
-var app = express();
-
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Mount routers AFTER creating app
+app.use('/restaurants', restaurantRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Catch 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
